5f79fd365a9543db01b1faefc42c45f61f2d84b9
juliano <juliano@INGSI08-PC.audi.d1>
2018-06-05 17:28:29 -0500

    Inicio Gestor.

--- gestor-web/src/main/java/com/gestor/web/ConvertidorGenerico.java
+++ gestor-web/src/main/java/com/gestor/web/ConvertidorGenerico.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.gestor.entity;
+package com.gestor.web;
 
 import java.util.Collections;
 import java.util.List;
